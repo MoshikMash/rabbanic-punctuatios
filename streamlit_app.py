5860e4c9909b0cd7732a8da8061d1ad3a716ec3f
MoshikMash <moshikmash@gmail.com>
2023-08-03 21:57:02 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ import os
 def main():
     st.markdown("<span style=\"background-color: #ccffcc;\">Green:</span> correct prediction – TP", unsafe_allow_html=True)
     st.markdown("<span style=\"background-color: #FFCCCC;\">Red:</span> Wrong prediction. Wherever there are square brackets (e.g. [.]), these square brackets represent the punctuation in the text, and the punctuation without brackets represents the prediction. If there is only punctuation within brackets and no punctuation without the brackets, it means that the prediction was BLANK (no punctuation), or, alternately, if there is no punctuation with brackets, and only punctuation without brackets, it means that there was no punctuation in the original text (only in the prediction). For example:", unsafe_allow_html=True)
-    st.markdown("[CLS] and [SEP] the beginning and the end of each chunk.)
+    st.markdown("[CLS] and [SEP] the beginning and the end of each chunk.")
     
     file_list = os.listdir('test_files')
     file_name = st.selectbox('Select File', file_list)
