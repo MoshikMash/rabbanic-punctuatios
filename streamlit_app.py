427311ea78eb7200191926b6e7d814982c2a6641
MoshikMash <moshikmash@gmail.com>
2023-08-03 22:07:51 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,13 +4,13 @@ import os
 
 
 def main():
-    st.markdown("## prediction description")
+    st.markdown("<u>Prediction Description</u>", unsafe_allow_html=True)
     st.markdown("<span style=\"background-color: #ccffcc;\">Green:</span> correct prediction – TP", unsafe_allow_html=True)
     st.markdown("<span style=\"background-color: #FFCCCC;\">Red:</span> Wrong prediction. square brackets (e.g. [.]) represent the punctuation in the original text (if any).", unsafe_allow_html=True)
     st.markdown("[CLS] and [SEP] the beginning and the end of each chunk.")
     
     file_list = os.listdir('test_files')
-    file_name = st.selectbox('Select File', file_list)
+    file_name = st.selectbox('Select File to Predict Punctuations', file_list)
 
     with open('test_files/' + file_name, 'rb') as file:
         data = pickle.load(file)
